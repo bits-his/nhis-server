ad279c9c1f5b77a7d62343ba9b7d8d0eebdd0936
Yasir Ado Hassan <ysquare.theimperial@gmail.com>
2023-05-17 11:43:53 +0100

    dialectchaged

--- src/models/index.js
+++ src/models/index.js
@@ -12,7 +12,7 @@ const config = {
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   host: process.env.DB_HOST,
-  dialect: process.env.DB_DIALECT,
+  dialect: 'mysql',
   pool: {
     max: 5,
     min: 0,
